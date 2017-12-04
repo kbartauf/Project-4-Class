3895997405b2766aa0f3d0f72b77b4065477d708
Ubuntu <kbarta@ufl.edu>
2017-12-04 12:47:41 -0800

    in progress #12

--- distributedFS.py
+++ distributedFS.py
@@ -1,39 +1,47 @@
 import xmlrpclib
 
-class Memory(LoggingMixIn, Operations):
+_SUCCESS = 0
+_FAILURE = 1
+
+class Memory():#LoggingMixIn, Operations):
 
     def __init__(self, iMetaPort, iaDataPorts):
-        self.meta_proxy = xmlrpclib.ServerProxy("http://localhost:"+str(metaport)+"/")
-        for iPort in iaDataPorts :
-            self.data_proxy_array[i] = xmlrpclib.ServerProxy("http://localhost:"+str(iPort)+"/")
+        self.meta_proxy = xmlrpclib.ServerProxy("http://localhost:"+str(iMetaport)+"/")
+        for i in len(iaDataPorts) :
+            self.data_proxy_array[i] = xmlrpclib.ServerProxy("http://localhost:"+str(iaDataPorts[i])+"/")
 
     def chmod(self, path, mode):
-        # Meta Server
+        self.meta_proxy.chmod(path, mode) # Meta Server
+        return 0
 
     def chown(self, path, uid, gid):
-        # Meta Server
+        self.meta_proxy.chown(path, uid, gid) # Meta Server
 
     def create(self, path, mode):
-        # Meta Server
+        self.meta_proxy.create(path, mode) # Meta Server
+        # [Data Server, Create Empty Lists]
+        return self.meta_proxy.open() # Meta Server
 
     def getattr(self, path, fh=None):
-        # Meta Server
+        attrs = self.meta_proxy.getattr(path)
+        if attrs == _FAILURE :
+            raise FuseOSError(ENOENT)
+        return attrs
 
     def getxattr(self, path, name, position=0):
-        # Meta Server
+        attrs = self.meta_proxy.getxattr(path, name) # Meta Server
+        if attrs == _FAILURE :
+            return '' # raise ENOATTR ?
+        return attrs
 
     def listxattr(self, path):
-        # Meta Server
+        return self.meta_proxy.listxattr(path) # Meta Server
 
     def mkdir(self, path, mode):
-         # Data Servers
-        startServer = startHash(path)
-        for i in data_server_array:
-            self.data_server_array[(startServer+i)%numServers].mklist(path)
-        # Meta Server
+        self.meta_proxy.mkdir(path, mode) # Meta Server
 
     def open(self, path, flags):
-        # Meta Server
+        return self.meta_proxy.open() # Meta Server
 
     def read(self, path, size, offset, fh):
          # Data Servers
